a981ac84710632d2ae8e2155143b839ae303ac60
Hîrbu Andrei <andrei_hrb@icloud.com>
2018-09-15 21:18:50 +0300

    init

---
+++ src/layouts/footer.php
@@ -0,0 +1,11 @@
+<?php
+
+echo '
+<footer class="footer">
+    <div class="container">
+		<p class="text-center text-muted">
+			&copy; 2018 ZOE (Hirbu Andrei)
+		</p>
+    </div>
+</footer>
+</div>';
