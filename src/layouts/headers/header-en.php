72b20f13393932374b765d664bd9aec623e5d0bd
Hîrbu Andrei <andrei_hrb@icloud.com>
2018-09-20 16:18:51 +0300

    stucture

---
+++ src/layouts/headers/header-en.php
@@ -0,0 +1,18 @@
+<?php echo '
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
+	<a class="navbar-brand" href="#"><img src="https://i.imgur.com/lg8lYEM.png" class="logo"></a>
+
+	<div class="ml-auto">
+	<a class="flag" href="home-fr.php">
+		<img class="resize mx-1" id="fr" src="https://i.imgur.com/BKIYTV3.png">
+	</a>
+
+	<a class="flag selectedEn" href="#">
+		<img class="resize mx-1 selected" id="en" src="https://i.imgur.com/lem1FL9.png">
+	</a>
+	
+	<a class="flag" href="home-ro.php">
+		<img class="resize mx-1" id="ro" src="https://i.imgur.com/L8CahZb.png">
+	</a>
+	</div>
+</nav> ';
---
+++ src/layouts/headers/header-ro.php
@@ -0,0 +1,18 @@
+<?php echo '
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
+	<a class="navbar-brand" href="#"><img src="https://i.imgur.com/lg8lYEM.png" class="logo"></a>
+
+	<div class="ml-auto">
+	<a class="flag" href="home-fr.php">
+		<img class="resize mx-1" id="fr" src="https://i.imgur.com/BKIYTV3.png">
+	</a>
+
+	<a class="flag" href="home-en.php">
+		<img class="resize mx-1" id="en" src="https://i.imgur.com/lem1FL9.png">
+	</a>
+	
+	<a class="flag selectedRo" href="#">
+		<img class="resize mx-1 selected" id="ro" src="https://i.imgur.com/L8CahZb.png">
+	</a>
+	</div>
+</nav> ';
