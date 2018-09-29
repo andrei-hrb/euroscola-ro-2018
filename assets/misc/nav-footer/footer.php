a3954cded4556dfbbd141045d120b1257030ef2f
Hîrbu Andrei <andrei_hrb@icloud.com>
2018-09-30 00:29:10 +0300

    self care

--- assets/misc/nav-footer/footer.php
+++ assets/misc/nav-footer/footer.php
@@ -1,11 +1,10 @@
 <hr>
+<div class="mt-5 bg-dark">
 	<div class="container">
 		<div class="row">
-			<div class="col-6">
-				<?php include('map.php'); ?>
-			</div>
-			<div class="col-6">
+			<div class="col-6 m-3">
 				<?php include('facebook.php'); ?>
 			</div>
 		</div>
 	</div>
+</div>
