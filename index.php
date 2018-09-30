b70940688f00efa21e8c6641fced2d5e0c44287e
Hîrbu Andrei <andrei_hrb@icloud.com>
2018-09-30 08:34:24 +0300

    test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126651509-1"></script>
 	<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-126651509-1');</script>
 	
-	<?php $lang=substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);?>
+	<?php $lang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);?>
 	<script>window.location.replace("home.php/?lang=<?= $lang ?>");</script>
 </head>
 
